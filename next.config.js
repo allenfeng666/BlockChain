d8fec00c7ca4749f03f13c0ba29fd3f3ad1e3709
Allen Feng <clfeng@uw.edu>
2022-10-17 16:05:24 -0700

    Deploying to Netlify

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  reactStrictMode: true,
+const nextConfig = {
+  reactStrictMode: true,
   images: {
+    loader: "akamai",
+    path: "",
     domains: [
-      'lh3.googleusercontent.com',
-      'openseauserdata.com',
-      'brand.assets.adidas.com',
-      'media0.giphy.com',
-      'avatars.dicebear.com',
+      "lh3.googleusercontent.com",
+      "openseauserdata.com",
+      "brand.assets.adidas.com",
+      "media0.giphy.com",
+      "avatars.dicebear.com",
       "media.giphy.com",
-      'media1.giphy.com',
-      'media3.giphy.com',
-      'media2.giphy.com',
-      'media4.giphy.com',
+      "media1.giphy.com",
+      "media3.giphy.com",
+      "media2.giphy.com",
+      "media4.giphy.com",
     ],
   },
-}
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
